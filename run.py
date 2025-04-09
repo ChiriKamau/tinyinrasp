48f6b84516063d7250c49b0109617fbfed28ccfc
Levis Chiri <120241319+ChiriKamau@users.noreply.github.com>
2025-04-09 10:29:15 +0300

    added a Model_quant

--- run.py
+++ run.py
@@ -1,31 +1,16 @@
-import numpy as np
-from PIL import Image
-from tflite_runtime.interpreter import Interpreter
+import tensorflow as tf
 
-# Load the TensorFlow Lite model
-interpreter = Interpreter(model_path='/home/pi/tinyinrasp/Model_quantized.tflite')
-interpreter.allocate_tensors()
+# Load your .h5 model
+model = tf.keras.models.load_model("your_model.h5")
 
-# Load labels
-with open('/home/pi/tflite_model/labels.txt', 'r') as f:
-    labels = [line.strip() for line in f.readlines()]
+# Convert to TFLite with quantization (reduces size + speeds up inference)
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+converter.optimizations = [tf.lite.Optimize.DEFAULT]  # Quantize weights
+converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]  # 8-bit
+converter.inference_input_type = tf.uint8  # Quantize input/output
 
-# Get input and output tensors
-input_details = interpreter.get_input_details()
-output_details = interpreter.get_output_details()
+tflite_model = converter.convert()
 
-# Function to preprocess input image
-def preprocess_image(image_path):
-    image = Image.open(image_path).resize((224, 224))
-    input_data = np.expand_dims(image, axis=0)
-    return np.array(input_data, dtype=np.float32)
-
-# Run inference on an image
-input_data = preprocess_image('/home/pi/tflite_model/test_image.jpg')
-interpreter.set_tensor(input_details[0]['index'], input_data)
-interpreter.invoke()
-
-# Get and print the output
-output_data = interpreter.get_tensor(output_details[0]['index'])
-predicted_class = np.argmax(output_data[0])
-print(f'Predicted class: {labels[predicted_class]}')
+# Save the quantized model
+with open("model_quant.tflite", "wb") as f:
+    f.write(tflite_model)
